c6d3f81ecec670a6a6c292ff45d0bd7456c4d57f
LuisMSuarez <140195810+LuisMSuarez@users.noreply.github.com>
2025-04-26 14:43:56 -0700

    keyvault integration

--- Services/GamesService.cs
+++ Services/GamesService.cs
@@ -1,4 +1,6 @@
-﻿using GameHubApi.Contracts;
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using GameHubApi.Contracts;
 using GameHubApi.Providers;
 
 namespace GameHubApi.Services
@@ -6,10 +8,16 @@ namespace GameHubApi.Services
     public class GamesService : IGamesService
     {
         private readonly RawgApi gamesProvider;
-
+        private const string KeyVaultUrl = "https://kv-gamers-hub.vault.azure.net/";
+        private const string SecretName = "RawgApiKey";
         public GamesService()
         {
-            gamesProvider = new RawgApi(new HttpClient(), "fetch from keyvault");
+            // Fetch the API key from Azure Key Vault
+            var client = new SecretClient(new Uri(KeyVaultUrl), new DefaultAzureCredential());
+            var secret = client.GetSecret(SecretName);
+            var apiKey = secret.Value.Value;
+
+            gamesProvider = new RawgApi(new HttpClient(), apiKey);
         }
         public async Task<CollectionResult<Game>> GetGamesAsync()
         {
