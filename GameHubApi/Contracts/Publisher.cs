80455c3c2d70493864643b6d3a7330a1072469dc
LuisMSuarez <140195810+LuisMSuarez@users.noreply.github.com>
2025-05-31 10:52:12 -0700

    Cleanup

--- GameHubApi/Contracts/Publisher.cs
+++ GameHubApi/Contracts/Publisher.cs
@@ -1,7 +1,7 @@
-﻿using System.Text.Json.Serialization;
-
-namespace GameHubApi.Contracts
+﻿namespace GameHubApi.Contracts
 {
+    using System.Text.Json.Serialization;
+
     public class Publisher
     {
         [JsonPropertyName("id")]
