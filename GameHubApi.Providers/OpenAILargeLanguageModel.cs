8105afd6b442e77497aed09d416260af602d777c
Luis M. Suarez <140195810+LuisMSuarez@users.noreply.github.com>
2025-10-01 11:44:59 -0700

    OpenAI nuget package, Interface and stub implementation (#41)

---
+++ GameHubApi.Providers/OpenAILargeLanguageModel.cs
@@ -0,0 +1,12 @@
+﻿using GameHubApi.Providers.Contracts;
+
+namespace GameHubApi.Providers
+{
+    internal class OpenAILargeLanguageModel : ILargeLanguageModel
+    {
+        public Task<GenerateResponseResult> GenerateResponseAsync(GenerateResponseQuery query)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
