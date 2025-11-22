b06f71feb1ad7015f7dd7fa53c2bec17f4535656
Luis M. Suarez <140195810+LuisMSuarez@users.noreply.github.com>
2025-11-21 23:11:42 -0800

    Login endpoint for OpenIdConnect flow (#59)
    This endpoint is intended for browsers only.
    It will trigger DefaultChallengeScheme, the authentication process using OpenIdConnect flow if the user is not authenticated.
    It will then set a cookie upon successful authentication.
    From this point on, the endpoints that use Cookies authentication scheme will work.
    The endpoints that allow both Cookies and Bearer authentication schemes only work if the user has a valid cookie or a valid Bearer token.
    They will not trigger the authentication process.

--- GameHubApi.Controllers/AdminController.cs
+++ GameHubApi.Controllers/AdminController.cs
@@ -7,22 +7,38 @@ namespace GameHubApi.Controllers
 {
     [ApiController]
     [Route("v1/[controller]")]
-    [Authorize(Policy = "MicrosoftOrBearer")]
     public class AdminController : ControllerBase
     {
-        [HttpGet("test")]
-        public IActionResult GetTestData()
+        [HttpGet("login")]
+        [Authorize]
+
+        // This endpoint is intended for browsers only.
+        // It will trigger DefaultChallengeScheme, the authentication process using OpenIdConnect flow if the user is not authenticated.
+        // It will then set a cookie upon successful authentication.
+        // From this point on, the endpoints that use Cookies authentication scheme will work.
+        // The endpoints that allow both Cookies and Bearer authentication schemes only work if the user has a valid cookie or a valid Bearer token.
+        // They will not trigger the authentication process.
+        public IActionResult Login()
         {
             var user = HttpContext.User;
-            var name = user.FindFirst(ClaimTypes.Name)?.Value ?? user.FindFirst("name")?.Value;
-            var email = user.FindFirst(ClaimTypes.Email)?.Value
-                ?? user.FindFirst("preferred_username")?.Value; // fallback for some Microsoft accounts
+            var name = user.FindFirst("name")?.Value;
+            var email = user.FindFirst("preferred_username")?.Value;
             var objectId = user.FindFirst("http://schemas.microsoft.com/identity/claims/objectidentifier")?.Value;
             return Ok(new
             {
                 name,
                 email,
                 objectId
+            });
+        }
+
+        [HttpGet("test")]
+        [Authorize(Policy = "CookiesAndBearer")]
+        public IActionResult GetTestData()
+        {
+            return Ok(new
+            { 
+                message = "You have accessed a protected endpoint using either Cookies or Bearer authentication." 
             });
         }
     }
