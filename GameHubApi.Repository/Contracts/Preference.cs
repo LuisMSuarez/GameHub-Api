7d11dc5bdbbc9bfe501ae9bce14f4c6746e4599f
LuisMSuarez <140195810+LuisMSuarez@users.noreply.github.com>
2025-11-24 16:18:12 -0800

    Created interfaces for UserGamesRepository

---
+++ GameHubApi.Repository/Contracts/Preference.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameHubApi.Repository.Contracts
+{
+    [Flags]
+    public enum Preference
+    {
+        Like,
+        Dislike,
+        Owned,
+        WishList
+    }
+}
